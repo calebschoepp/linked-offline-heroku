881ece2dac3a9a72864d22a1afe96960bce51a40
Caleb Schoepp <caleb.schoepp@gmail.com>
2019-11-26 00:00:41 -0700

    Added multiparty to parse forms

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const multiparty = require("multiparty");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("I am working"));
+app.post("/email", (req, res) => {
+  let form = new multiparty.Form();
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+  form.parse(req, (err, fields, files) => {
+    console.log(fields["stripped-html"][0]);
+    res.send("Received");
+  });
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
