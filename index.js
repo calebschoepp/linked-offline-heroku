9e46ba736539db9f58f47eb71634f869dedff6f7
Caleb Schoepp <caleb.schoepp@gmail.com>
2019-11-25 23:55:23 -0700

    Another fix for port

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const app = express();
 const PORT = process.env.PORT || 5000;
+
+const app = express();
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
