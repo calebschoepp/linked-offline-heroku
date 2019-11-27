c92f18acac3987c1ac15be0175b270ef9e371cf7
Caleb Schoepp <caleb.schoepp@gmail.com>
2019-11-26 17:29:13 -0700

    Try removing urlencoding middleware

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true, limit: "20 MB" }));
+// app.use(express.urlencoded({ extended: true, limit: "20 MB" }));
 
 app.get("/", (req, res) => res.send("I am working"));
 app.post("/email", (req, res) => {
