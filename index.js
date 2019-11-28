65990ce893a0f5f08a245084c1e9f81d2c7a407c
Caleb Schoepp <caleb.schoepp@gmail.com>
2019-11-27 23:29:38 -0700

    Handle url-encoded seperate from multipart

--- index.js
+++ index.js
@@ -4,11 +4,29 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-// app.use(express.urlencoded({ extended: true, limit: "20 MB" }));
+app.use(express.urlencoded({ extended: true, limit: "20 MB" }));
 
 app.get("/", (req, res) => res.send("I am working"));
 app.post("/email", (req, res) => {
-  console.log(req.headers["content-type"]);
+  console.log(`Parsing req with content-type ${req.headers["content-type"]}`);
+  if (req.is("application/x-www-form-urlencoded")) {
+    handleEncodedEmail(req, res);
+  } else if (req.is("multipart/form-data")) {
+    handleMultipartEmail(req, res);
+  } else {
+    res.status(415);
+    res.send("Unsupported content-type");
+  }
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
+
+function handleEncodedEmail(req, res) {
+  console.log(req.body);
+  res.send("Received");
+}
+
+function handleMultipartEmail(req, res) {
   let form = new multiparty.Form({ maxFieldsSize: 6e6 });
 
   form.parse(req, (err, fields, files) => {
@@ -25,6 +43,4 @@ app.post("/email", (req, res) => {
     }
     res.send("Received");
   });
-});
-
-app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
+}
